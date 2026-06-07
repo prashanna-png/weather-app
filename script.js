460be136bbbb87ee15338a69939a3f4de1c67822
prashanna <prashanna9742@gmail.com>
2026-06-07 21:28:52 +0545

    add api and fetch

---
+++ script.js
@@ -0,0 +1,41 @@
+const cityInput = document.getElementById('cityInput');
+const weatherCard = document.getElementById('weatherCard');
+const searchBtn = document.getElementById('searchBtn');
+const temperature = document.getElementById('temperature');
+const cityName = document.getElementById('cityName');
+const condition = document.getElementById('condition');
+const humidity = document.getElementById('humidity');
+
+searchBtn.addEventListener('click', function () {
+  const city = cityInput.value;
+  getWeather(city);
+});
+
+function getWeather(city) {
+  const apiKey = 'c971fdb53c4d659632fcc15992470915';
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+  cityName.textContent = 'Loading...';
+
+  fetch(url)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      if (data.cod === '404') {
+        cityName.textContext = 'city not found!!';
+        return;
+      }
+      console.log(data);
+      cityName.textContent = data.name;
+      temperature.textContent = data.main.temp + '°C';
+      condition.textContent = data.weather[0].description;
+      humidity.textContent = 'Humidity: ' + data.main.humidity + '%';
+    })
+}
+
+cityInput.addEventListener('keypress', function (e) {
+  if (e.key === 'Enter') {
+    getWeather(cityInput.value);
+  }
+})
